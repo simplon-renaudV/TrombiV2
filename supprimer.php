9489a0472f2058ee56cbc4c2ed5025a9d1a742ad
Renaud Vivancos <renaud.vivancos@free.fr>
2015-11-08 18:10:28 +0100

    qqes modifs sur le fichier supprimer.php

--- supprimer.php
+++ supprimer.php
@@ -1,9 +1,25 @@
-<meta http-equiv="refresh" content="2;Trombinoscope.php" />
+<!DOCTYPE html>
+<html lang="fr">
 
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="refresh" content="2;Trombinoscope.php" />
+      <title>Trombinoscope</title>
+      <link rel="stylesheet" href="style.css"/>
+      <link href='https://fonts.googleapis.com/css?family=Cabin:600italic' rel='stylesheet' type='text/css'>
+   </head>
 
-<?php
-   include("../../configPDO.php"); //recupère les infos de connexions a la bdd
+   <body>
 
-   $req = $bdd->prepare('DELETE FROM simploniens WHERE id = :suppr');
-   $req->execute(array('suppr' => $_GET['id']));
-?>
+      <?php
+   
+         include("../../configPDO.php"); //recupère les infos de connexions a la bdd
+
+         $req = $bdd->prepare('DELETE FROM simploniens WHERE id = :suppr');
+         $req->execute(array('suppr' => $_GET['id']));
+      
+      ?>
+   
+   </body>
+
+</html>
